e7b44a996e609c1dec458695b5686bf9a73fb8f1
anukuljoshi <anukuljoshi1@gmail.com>
2024-06-05 00:08:58 +0530

    added get and set methods

--- app/commands.py
+++ app/commands.py
@@ -2,6 +2,8 @@ from typing import Any
 
 from parser.parser import RESPParser
 
+STORE = dict()
+
 
 class CommandAction:
     """
@@ -13,8 +15,28 @@ class CommandAction:
         return parser.encode("PONG")
 
     @staticmethod
-    def echo_action(parser: RESPParser, message: Any):
+    def echo_action(parser: RESPParser, *args):
+        if len(args) != 1:
+            return parser.encode("ECHO takes in 1 argument")
+        message = args[0]
         return parser.encode(message)
+
+    @staticmethod
+    def set_action(parser: RESPParser, key: Any, value):
+        if type(key) is not type(""):
+            return parser.encode("Key must be a string")
+        STORE[key] = value
+        return parser.encode("OK")
+
+    @staticmethod
+    def get_action(parser: RESPParser, key: str):
+        if type(key) is not type(""):
+            return parser.encode("Key must be a string")
+
+        value = STORE.get(key, None)
+        if value is None:
+            return parser.encode(None)
+        return parser.encode(value)
 
     @staticmethod
     def unknown_action(parser: RESPParser):
@@ -24,6 +46,8 @@ class CommandAction:
 class Command:
     PING = "ping"
     ECHO = "echo"
+    SET = "set"
+    GET = "get"
     UNKNOWN = "unknown"
 
     @staticmethod
@@ -32,5 +56,9 @@ class Command:
             return CommandAction.ping_action
         elif command.lower() == Command.ECHO:
             return CommandAction.echo_action
+        elif command.lower() == Command.GET:
+            return CommandAction.get_action
+        elif command.lower() == Command.SET:
+            return CommandAction.set_action
         else:
             return CommandAction.unknown_action
